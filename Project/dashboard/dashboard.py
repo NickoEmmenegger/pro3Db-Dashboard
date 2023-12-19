492237ecff1e6875589d7229fed150827c130fc4
nicko.emmenegger <nicko.emmenegger@students.fhnw.ch>
2023-12-19 11:23:25 +0100

    Update dashboard.py

--- Project/dashboard/dashboard.py
+++ Project/dashboard/dashboard.py
@@ -13,5 +13,8 @@ uploaded_file = st.file_uploader("Upload file", type=['csv', 'parquet'])
 # Load the PNG image using PIL (Python Imaging Library)
 prediction_plot = Image.open('cycle_44.png')
 
+# Adding a caption
+st.markdown("<center>Input data and prediction for Cycle 44</center>", unsafe_allow_html=True)
+
 # Display the image in Streamlit
-st.image(prediction_plot, caption='Input data and prediction for Cycle 44',use_column_width=True)
+st.image(prediction_plot,use_column_width=True)
